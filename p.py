74ef12ecd7d30cf1fbb9a38767ad50f09476d3ec
udhay <rimpighosh1530@gmail.com>
2025-08-01 19:46:33 +0530

    Update p.py

--- p.py
+++ p.py
@@ -4,8 +4,8 @@ while True:
     print("+--------------------+")
     print("| 1. add             |")
     print("| 2. view            |")
-    print("| 3. update          |")
-    print("| 4. delete          |")
+    print("| 3. update          |")#
+    print("| 4. delete          |")#
     print("| 5. Exit            |")
     print("+--------------------+")
         
@@ -14,38 +14,36 @@ while True:
         print("| adding operations |")
         print("| 1. student detail |")
         print("| 2. mark entry     |")
-        print("| 3. exit           |")
-        sub_choice = int(input("Enter your choice (1-3): "))
+        
+        sub_choice = int(input("Enter your choice (1-2): "))
         if sub_choice == 1:
-            print(" student detail  ")
+            add_student()
         elif sub_choice == 2:
-            print(" mark entry ")
-        elif sub_choice == 3:
-            print("Returning to main menu...")
+            mark_entry()
+        
         else:
             print("Invalid choice in submenu.")
     elif choice == 2:
         print("| 1. student detail |")
         print("| 2. mark           |")
-        print("| 3. exit           |")
-        sub_choice = int(input("Enter your choice (1-3): "))
+        
+        sub_choice = int(input("Enter your choice (1-2): "))
         if sub_choice == 1:
-            print("View student detail ")
+            view_student()
         elif sub_choice == 2:
-            print("View mark ")
-        elif sub_choice == 3:
-            print("Returning to main menu...")
+            view_marks()
+        
         else:
             print("Invalid choice in submenu.")
     elif choice == 3:
         print("| 1. update student detail |")
         print("| 2. mark update           |")
-        print("| 3. exit                  |")
-        sub_choice = int(input("Enter your choice (1-3): "))
+        
+        sub_choice = int(input("Enter your choice (1-2): "))
         if sub_choice == 1:
-            print("Update student detail")
+            print("Update student detail")# 
         elif sub_choice == 2:
-            print("Update mark ")
+            print("Update mark ")  #         
         elif sub_choice == 3:
             print("Returning to main menu...")
         else:
@@ -53,14 +51,13 @@ while True:
     elif choice == 4:
         print("| 1. delete student detail |")
         print("| 2. mark delete           |")
-        print("| 3. exit                  |")
-        sub_choice = int(input("Enter your choice (1-3): "))
+        
+        sub_choice = int(input("Enter your choice (1-2): "))
         if sub_choice == 1:
-            print("Delete student detail ")
+            print("Delete student detail ") #
         elif sub_choice == 2:
-            print("Delete mark ")
-        elif sub_choice == 3:
-            print("Returning to main menu...")
+            print("Delete mark ")           #
+        
         else:
             print("Invalid choice in submenu.")
     elif choice == 5:
@@ -68,3 +65,45 @@ while True:
         break
     else:
         print("Invalid choice. Please enter a number from 1 to 5.")
+##############################################################################################################################################################################        
+def add_student():
+    f= open("student.txt", "a")
+    a= int(input("Enter the number of students to add: "))
+    for i in range(a):
+        name = input("Enter student name: ")
+        roll_no = input("Enter roll number: ")
+        class_name = input("Enter class name: ")
+        extra = input("Enter extra information: ")
+        f.write(f"{name} {roll_no} {class_name} {extra}\n")
+    f.close() 
+def mark_entry():
+    f = open("marks.txt", "a")
+    a = int(input("Enter the number of students to add marks for: "))
+    for i in range(a):
+        roll_no = input("Enter roll number: ")
+        print("Enter subject and marks in the format 'subject marks':")
+        grade=input("enter subject and marks: ")
+        f.write(f"{roll_no} {grade}\n")
+    f.close()
+    print("Marks added successfully.")    
+def view_student():
+    f= open("student.txt", "r")
+    s=input("enter the name of student to view: ")
+    for line in f:
+        word=line.split()
+        if word[0] == s:
+            print("Student details:", line)
+            return
+    print("Student not found.")
+    f.close()
+def view_marks(): 
+    f= open("marks.txt", "r")
+    n=input("enter the roll no. of student to view: ")
+    for line in f:
+        roll_no=line.split()
+        if roll_no[0] == n:
+            print("Student MARKS:", line)
+            return
+    print("Student not found.")
+    f.close()
+    
